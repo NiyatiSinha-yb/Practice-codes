ac783d6de1d191beebfb0e3fe309b49ced6d2300
Niyati Sinha <niyati.sinha29@gmail.com>
2021-04-03 01:39:02 +0530

    Create subset.java
    Code exceeds time limit

---
+++ Leetcode/April-LeetCode-Challenge-2021/2.Ones and Zeroes/subset.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        
+        int max=0;
+        for(int i=0; i<(1<<strs.length) ;i++){ // a<<b means in binary shft where we shift the binarry bits of a to b bits right adding 0's in the end 
+            
+   //    **********************IMPORTANT*********************  
+            
+   //         1 -> binary =  0001   =                 0*2^1 + 1*2^0   =   1*2^0
+   //      1<<3 -> binary =  1000   = 1*2^3 + 0*2^2 + 0*2^1 + 0*2^0   =   1*2^3
+            
+    //thus basically 1<<a means 2^a in binary system i.e. only a-th bit as 1
+    // if we were using octal system it would have been 8^a and so on
+            
+            String completeSubString = "";
+            int count=0;
+            
+            for( int j=0; j<strs.length ;j++)
+            {
+                if( ( i & (1<<j)) > 0 ) // a & b ->convert a & b to binary and perform 'AND' operation on binary digits i.e. T&T=T T&F=F F&T=F F&F=F
+                {
+                    //System.out.print(strs[j]+",");
+                    completeSubString +=strs[j];
+                    count++;
+                }
+                
+            }
+            //System.out.println();
+            
+            //System.out.println(completeSubString);
+            if(findCount(completeSubString,m,n)==1)
+                if(count>max)
+                    max=count;
+        }
+        return max;
+    }
+            
+     int findCount(String str, int m, int n)    
+     {
+       
+            int totalLength = str.length();
+            String copy=str;
+            
+            
+            if(copy.length()-(copy.replace("1","")).length()<=n)
+            {
+                //copy=str;
+                if(copy.length()-(copy.replace("0","")).length()<=m)
+                {
+                    return 1;
+                }
+            }
+         
+         return 0;
+  
+        }
+    
+       
+}
