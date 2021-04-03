b7008c34d695247fa06455c69f7cf47b81162bea
Niyati Sinha <niyati.sinha29@gmail.com>
2021-04-03 08:56:07 +0530

    Create Operators.java

---
+++ interviewBit/FastTrackCourses/Java/Operators.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.util.*;
+
+public class Operators {
+    public static void main(String[] args) {
+    
+    /***Don't change anything here***/
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt();
+        inp.nextLine();
+        int b = inp.nextInt();
+        inp.nextLine();
+        inp.close();
+    /*********************************/
+    
+    /*Perform the task here*/
+        
+        int add=a+b;
+        int sub=a-b;
+        int multi=a*b;
+        int div=a/b;
+        
+    /***********************/
+    
+    /******Don't change anything here******/
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+    
+    }
+}
