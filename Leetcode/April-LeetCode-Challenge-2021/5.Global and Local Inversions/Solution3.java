e60ef0f83e8ae11b7e12f619cb3946e198da7079
Niyati Sinha <niyati.sinha29@gmail.com>
2021-04-05 16:10:08 +0530

    Beats 99% of codes in memory usage

---
+++ Leetcode/April-LeetCode-Challenge-2021/5.Global and Local Inversions/Solution3.java
@@ -0,0 +1,26 @@
+class Solution3 {
+    public boolean isIdealPermutation(int[] A) {
+        int global=0;
+        //int local=0;
+        int n= A.length;
+        
+        for(int i=0; i<n-1; i++){
+            
+            for(int j=i+1; j<n; j++){
+                if(A[i]>A[j])
+                    global++;
+            }
+            
+            if(A[i]>A[i+1])
+                global--;
+            
+        }
+        
+        A=null;
+        
+        if(global==0)
+            return true;
+        return false;
+        
+    }
+}
