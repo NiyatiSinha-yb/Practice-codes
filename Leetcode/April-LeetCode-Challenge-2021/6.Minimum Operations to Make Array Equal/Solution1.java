18527eb8fa70851ee5d57d820388eba966365630
Niyati Sinha <niyati.sinha29@gmail.com>
2021-04-06 23:59:00 +0530

    runtime beats 100.00%, memory usage beats 97.13 java codes

---
+++ Leetcode/April-LeetCode-Challenge-2021/6.Minimum Operations to Make Array Equal/Solution1.java
@@ -0,0 +1,22 @@
+class Solution1 {
+   
+    public int minOperations(int n) {
+    
+        /* applicable only for even number of n i.e. even number of nodes
+        for(int i=0; i<n/2; i++){
+            count=count+2*i+1;
+        }
+        */
+        
+        if(n%2==0)
+            return (n/2)*(n/2);
+        else
+            return (n/2)*(n/2+1);
+        
+    }
+}
+
+/*
+Your runtime beats 100.00 % of java submissions
+Your memory usage beats 97.13 % of java submissions
+*/
