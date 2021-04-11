73ee13b55a59927f32fedfa3b39cc604d2242330
Niyati Sinha <niyati.sinha29@gmail.com>
2021-04-11 12:15:53 +0530

    Create Solution.java

---
+++ Leetcode/Contests/1822. Sign of the Product of an Array/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int neg=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0)
+                return 0;
+            else if (nums[i]<0)
+                neg++;
+        }
+        if(neg%2==0)
+            return 1;
+        else
+            return -1;
+                
+        
+        
+    }
+}
