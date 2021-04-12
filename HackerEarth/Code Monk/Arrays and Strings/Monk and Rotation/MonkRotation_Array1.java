b72a208bf998f45f84d9925c37f43567b0c5e6cf
Niyati Sinha <niyati.sinha29@gmail.com>
2021-04-12 23:32:23 +0530

    Create MonkRotation_Array1.java

---
+++ HackerEarth/Code Monk/Arrays and Strings/Monk and Rotation/MonkRotation_Array1.java
@@ -0,0 +1,35 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+import java.util.Scanner;
+class MonkRotation_Array1{
+    
+    static Scanner sc = new Scanner (System.in);
+
+    static void makeArray(int size, int k){
+        sc.nextLine();
+        int arr[]=new int[size];
+        String content= sc.nextLine();
+        String[] elements= content.split(" ");
+        int len=0;
+
+        for(int i=k;i<size;i++)
+            arr[i]=Integer.parseInt(elements[len++]);
+
+        for(int i=0;i<k;i++)
+            arr[i]=Integer.parseInt(elements[len++]);
+
+        for(int i=0; i<size ; i++)
+            System.out.print(arr[i]+" ");
+    }
+    public static void main(String args[]){
+        
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            makeArray(sc.nextInt(), sc.nextInt());
+        } //int size= sc.nextInt();
+            //int k=sc.nextInt();
+            //makeArray(size,k);
+    }
+}
+
+
