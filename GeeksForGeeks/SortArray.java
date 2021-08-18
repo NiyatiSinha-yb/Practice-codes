5384526ec59b54dad4788a5d8e313906fee8a0aa
Niyati Sinha <niyati.sinha29@gmail.com>
2021-08-18 19:13:22 +0530

    Create SortArray.java
    https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1#

---
+++ GeeksForGeeks/SortArray.java
@@ -0,0 +1,34 @@
+class SortArray
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+      
+        int zeroCount=0, oneCount=0, twoCount=2;
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]==0)
+                zeroCount++;
+            else if(a[i]==1)
+                oneCount++;
+            else
+                twoCount++;
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(zeroCount>0){
+                a[i]=0;
+                zeroCount--;
+            }
+            else if(oneCount>0){
+                a[i]=1;
+                oneCount--;
+            }
+            else{
+                a[i]=2;
+                twoCount--;
+            }
+        }
+
+    }
+}
