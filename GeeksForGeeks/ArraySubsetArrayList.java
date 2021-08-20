95f48c07be48eed9c624b6fafb1693162194d88b
Niyati Sinha <niyati.sinha29@gmail.com>
2021-08-21 00:17:25 +0530

    Create ArraySubsetArrayList.java

---
+++ GeeksForGeeks/ArraySubsetArrayList.java
@@ -0,0 +1,20 @@
+//Time Complexity :O(Math.max(a1.length, (a2.length)^2)
+//Space Complexity: O(a1.length)
+//https://practice.geeksforgeeks.org/problems/array-subset-of-another-array2317/1
+//User function Template for Java
+
+
+class ArraySubsetArrayList {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        if(a1.length<a2.length)
+            return "No";
+        ArrayList<Long> ele=new ArrayList<>();
+        for(int i=0; i<a1.length; i++)
+            ele.add(a1[i]);
+        for(int i=0; i<a2.length; i++)
+            if(!ele.contains(a2[i])) //contains O(a2.length)
+                return "No";
+        return "Yes";
+        
+    }
+}
