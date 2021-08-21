8dbc44a7afc4a1ff85fa3b88d1cdfd50d6bfe251
Niyati Sinha <niyati.sinha29@gmail.com>
2021-08-21 12:10:09 +0530

    Create DuplicateNo.java

---
+++ Leetcode/DuplicateNo.java
@@ -0,0 +1,14 @@
+class DuplicateNo {
+    public int findDuplicate(int[] arr) {
+        
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length/2; i++)
+        {
+            if(arr[i]==arr[i+1])
+                return arr[i];
+            if(arr[(arr.length-1)-i]==arr[(arr.length-1)-i-1])
+                return arr[arr.length-1-i];
+        }
+        return 1;
+    }
+}
