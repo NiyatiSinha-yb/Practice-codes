fd17bace8328f10e5a130f39b9c3a0f75569be5d
Niyati Sinha <niyati.sinha29@gmail.com>
2021-08-24 11:23:38 +0530

    Create ShuffleString3.java
    Convert char array to String using: new String(charArrayName)

---
+++ Leetcode/ShuffleString3.java
@@ -0,0 +1,13 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+//https://leetcode.com/problems/shuffle-string/submissions/
+class ShuffleString3 {
+    public String restoreString(String s, int[] indices) {
+        char arr[]=new char[s.length()];
+        for(int i=0; i<s.length();i++)
+        {
+            arr[indices[i]]=s.charAt(i);
+        }
+        return new String(arr);
+    }
+}
