91d8aff77aa2fc384f57c829b8d49d290ba23013
Niyati Sinha <niyati.sinha29@gmail.com>
2021-10-01 10:34:19 +0530

    Height Checker
    Height Checker

---
+++ Leetcode/HeightChecker.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+//Time Complexity: O(n log n)
+//Space Complexity: O(n)
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int expectedHeight[]=heights.clone();
+        
+        /*for(int i=0; i<heights.length; i++)
+            System.out.println(expectedHeight[i]);
+        */
+        
+        Arrays.sort(expectedHeight);
+       
+        /* for(int i=0; i<heights.length; i++)
+            System.out.println("* "+expectedHeight[i]+" -- "+heights[i]);
+        */
+        
+        int count=0;
+        for(int i=0; i<heights.length; i++)
+            if(expectedHeight[i]!=heights[i])
+                count++;
+        
+        //System.out.println(expectedHeight);
+        return count;
+    }
+}
