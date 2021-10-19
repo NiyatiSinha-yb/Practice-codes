cc25add69189730e5b612c2d663c2fb87bfd73e5
Niyati Sinha <niyati.sinha29@gmail.com>
2021-10-19 23:27:18 +0530

    28. Implement strStr()

---
+++ Leetcode/ImplementstrStr.java
@@ -0,0 +1,10 @@
+//28. Implement strStr()
+//https://leetcode.com/problems/implement-strstr/
+//time Complexirty: O(n)
+//Space complexity: O(1)
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
