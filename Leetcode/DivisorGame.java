c4f8f0b5ea73388a791aaece3689c142c7a2f1ec
Niyati Sinha <niyati.sinha29@gmail.com>
2021-10-26 14:10:34 +0530

    1025. Divisor Game

---
+++ Leetcode/DivisorGame.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/divisor-game/submissions/
+//1025. Divisor Game
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+
+class Solution {
+    public boolean divisorGame(int n) {
+        //Alice
+        if(n%2==0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
