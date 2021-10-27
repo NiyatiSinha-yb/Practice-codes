95cf48ad2ea07fba6f2f10ae72166a41af170bf3
Niyati Sinha <niyati.sinha29@gmail.com>
2021-10-27 14:07:35 +0530

    70. Climbing Stairs

---
+++ Leetcode/ClimbingStairs2.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/climbing-stairs/submissions/
+//70. Climbing Stairs
+//Time Complexity: O(n)
+//Space Compplexity: O(n)
+
+class Solution {
+    public int climbStairs(int n) {
+        /*
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        return climbStairs(n-1)+climbStairs(n-2);
+        */
+        
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
