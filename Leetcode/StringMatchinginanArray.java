468fa704778d1c64c4fbc6d39e5542da95b2a8eb
Niyati Sinha <niyati.sinha29@gmail.com>
2021-11-05 23:19:41 +0530

    1408. String Matching in an Array

---
+++ Leetcode/StringMatchinginanArray.java
@@ -0,0 +1,25 @@
+//time Complexity: O(n^3 log n)
+//Space complexity: O(n)
+//https://leetcode.com/problems/string-matching-in-an-array/submissions/
+//1408. String Matching in an Array
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List <String> res= new ArrayList<String>();
+        for(int i=0; i<words.length; i++)
+        {
+            for(int j=0; j<words.length; j++)
+            {
+                if(i==j)
+                    continue;
+                if(words[j].contains(words[i]))
+                {
+                    if(!res.contains(words[i]))
+                        res.add(words[i]);
+                }
+                    
+            }
+        }
+        return res;
+    }
+}
