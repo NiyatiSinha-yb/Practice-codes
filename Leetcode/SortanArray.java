3932f3b352c125cf3852d59c2bdf4e9fa85bc67e
Niyati Sinha <niyati.sinha29@gmail.com>
2021-11-13 06:26:16 +0530

    912. Sort an Array

---
+++ Leetcode/SortanArray.java
@@ -0,0 +1,11 @@
+//Time complexity: O(N log N)
+//Space Complexity: O(Log N)
+//912. Sort an Array
+//https://leetcode.com/problems/sort-an-array/
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
