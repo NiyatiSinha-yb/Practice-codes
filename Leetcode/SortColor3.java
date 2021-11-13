262bfa79eb82b45c27b5d442683d710fb65bec58
Niyati Sinha <niyati.sinha29@gmail.com>
2021-11-13 06:37:21 +0530

    75. Sort Colors

---
+++ Leetcode/SortColor3.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/sort-colors/
+//75. Sort Colors
+//time Complexty: O(n log n)
+//Space complexity: O(log n)
+
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        
+    }
+}
