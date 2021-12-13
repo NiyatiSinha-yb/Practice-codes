4d6e9f8d00787d09bba16b2aa44b2535673ced70
Niyati Sinha <niyati.sinha29@gmail.com>
2021-12-13 16:45:30 +0530

    100. Same Tree

---
+++ Leetcode/SameTree.java
@@ -0,0 +1,38 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+//https://leetcode.com/problems/same-tree/submissions/
+//100. Same Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p ==null && q==null)
+            return true;
+        if((p==null && q!=null)||(p!=null && q==null))
+            return false;
+        if(p.val != q.val)
+            return false;
+        if(isSameTree(p.left,q.left)==false)
+            return false;
+        
+        if(isSameTree(p.right,q.right)==false)
+            return false;
+        
+        return true;
+        
+    }
+}
