08ec8dad8d586665227308024c7dc80d9f386caa
Niyati Sinha <niyati.sinha29@gmail.com>
2021-12-25 23:35:55 +0530

    Create ValidPerfectSquare.java

---
+++ Leetcode/ValidPerfectSquare.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/valid-perfect-square/
+//367. Valid Perfect Square
+//Time Complexity: O(Math.sqrt(n))
+//Spcae Complexity: O(1)
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int n=1;
+        while(n<=num)
+        {
+            if(n>>2)
+        }
+        return false;
+    }
+}
