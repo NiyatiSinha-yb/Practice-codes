ac433911c6ea5091b667e7792f7af7b0c279072d
Niyati Sinha <niyati.sinha29@gmail.com>
2022-02-19 20:23:51 +0530

    5997. Find Three Consecutive Integers That Sum to a Given Number

---
+++ Leetcode/FindThreeConsecutiveIntegersThatSumtoaGivenNumber.java
@@ -0,0 +1,18 @@
+//5997. Find Three Consecutive Integers That Sum to a Given Number
+
+class Solution {
+    public long[] sumOfThree(long num) {
+        long res[] =new long[3];
+            
+            if(num%3==0)
+            {
+                res[0]=num/3-1;
+                res[1]=num/3;
+                res[2]=num/3+1;
+                return res;
+            }
+        
+        return new long[0];
+            
+    }
+}
