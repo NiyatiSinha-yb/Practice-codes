41ab7586cb2ac2c7bf0c40735412da94bc4e4c20
Niyati Sinha <niyati.sinha29@gmail.com>
2022-03-18 15:22:03 +0530

    709. To Lower Case

---
+++ Leetcode/ToLowerCase3.java
@@ -0,0 +1,10 @@
+//Space Complexity: O(1)
+//Time Complexity: O(n)
+//709. To Lower Case
+//https://leetcode.com/problems/to-lower-case/
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
