fd391dbeacfe16c22f30f17aad2bf3d0afa3659f
Niyati Sinha <niyati.sinha29@gmail.com>
2022-05-21 20:11:31 +0530

    1757. Recyclable and Low Fat Products
    RecyclableAndLowFatProducts.sql

---
+++ RecyclableAndLowFatProducts.sql
@@ -0,0 +1,6 @@
+#1757. Recyclable and Low Fat Products
+#https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+
+# Write your MySQL query statement below
+SELECT product_id FROM Products where low_fats='Y' and recyclable='Y';
