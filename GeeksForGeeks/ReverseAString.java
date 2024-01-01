1cb3c865e8718cd0555a4d5941bbbe4bf1ec44bd
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-01 14:29:20 +0530

    Create ReverseAString.java

---
+++ GeeksForGeeks/ReverseAString.java
@@ -0,0 +1,37 @@
+//https://www.geeksforgeeks.org/problems/java-reverse-a-string0416/1?page=1&difficulty=School&status=unsolved&sortBy=submissions
+//Reverse a String
+//Time Complexity: O()
+//Space Complexity: O()
+
+//{ Driver Code Starts
+// Initial template for Java
+
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.revStr(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static String revStr(String S) {
+        // code here
+        String str="";
+        for(int i=S.length()-1; i>=0; i--)
+        {
+            str+=S.charAt(i);
+        }
+        return str;
+    }
+}
