c5a459712d582306e9011a0db583cd2b0c104b94
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-02 22:43:44 +0530

    Create SumOfNumbersSet1.java

---
+++ GeeksForGeeks/SumOfNumbersSet1.java
@@ -0,0 +1,39 @@
+//C++ Functions (Sum of numbers) | Set 1
+//https://www.geeksforgeeks.org/problems/c-functions-set-1introduction/1?page=1&difficulty=School&status=unsolved&sortBy=accuracy
+//Time Complexity: O(1)
+//Space complexity: O(1)
+
+
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+int calcSum(int a,int b,int c);
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int sum = calcSum(a,b,c);
+	    cout<<sum<<"\n";
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*  Write your function here */
+
+/* The function should be named calcSum
+    and accepts three parameters of integer type
+    and returns the sum of the three integers */
+    
+int calcSum(int a, int b, int c)    
+{
+    // Your code here
+    return a+b+c;
+}
