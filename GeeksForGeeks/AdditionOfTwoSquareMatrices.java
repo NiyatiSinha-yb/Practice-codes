b215681506ea160260e7fcd825239c3f55480a18
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-03 07:34:53 +0530

    Create AdditionOfTwoSquareMatrices.java

---
+++ GeeksForGeeks/AdditionOfTwoSquareMatrices.java
@@ -0,0 +1,64 @@
+//Addition of two square matrices
+//https://www.geeksforgeeks.org/problems/addition-of-two-square-matrices4916/1?page=1&difficulty=School&status=unsolved&sortBy=accuracy
+//Time Complexity: O(1)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrixA = new int[n][n];
+            int[][] matrixB = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrixA[i][j] = Integer.parseInt(S[j]);
+            }
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrixB[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            ob.Addition(matrixA, matrixB);
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    System.out.print(matrixA[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public void  Addition(int[][] matrixA, int[][] matrixB)
+    {
+        int len=matrixA.length;
+        // code here
+        for(int i=0; i<len; i++)
+        {
+            for(int j=0; j<len; j++)
+            {
+                matrixA[i][j]=matrixA[i][j]+matrixB[i][j];
+            }
+        }
+    }
+}
