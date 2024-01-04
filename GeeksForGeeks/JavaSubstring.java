311849c23acffa0c37c571a8ea09bef8ee291f5b
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-04 11:23:45 +0530

    Create JavaSubstring.java

---
+++ GeeksForGeeks/JavaSubstring.java
@@ -0,0 +1,33 @@
+//Java Substring
+//https://www.geeksforgeeks.org/problems/java-substring5058/1?page=1&difficulty=School&status=unsolved&sortBy=accuracy
+//Time Complexity: O(n)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+// Initial template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            String S = in.next();
+            int L = in.nextInt();
+            int R = in.nextInt();
+            Solution ob = new Solution();
+
+            System.out.println(ob.javaSub(S, L, R));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for Java
+class Solution {
+    static String javaSub(String S, int L, int R) {
+        // code here
+        return S.substring(L,R+1);
+    }
+}
