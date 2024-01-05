474a5ef8c4f3c8731175ac4625191e574e3b1e0d
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-05 09:21:36 +0530

    Create C++SwitchCaseStatement.cpp

---
+++ GeeksForGeeks/C++SwitchCaseStatement.cpp
@@ -0,0 +1,46 @@
+//C++ Switch Case Statement
+//https://www.geeksforgeeks.org/problems/c-switch-case-statement5900/1?page=3&difficulty=School&status=unsolved&sortBy=accuracy
+//Time Complexity: O(1)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isInRange(int N){
+        // code here 
+        
+        switch(N){
+            case 1: return "one";
+            case 2: return "two";
+            case 3: return "three";
+            case 4: return "four";
+            case 5: return "five";
+            case 6: return "six";
+            case 7: return "seven";
+            case 8: return "eight";
+            case 9: return "nine";
+            case 10: return "ten";
+            default: return "not in range";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isInRange(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
