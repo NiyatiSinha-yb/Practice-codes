0be23dedb00f532a596d8a0b1af556793d7f151b
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-09 12:22:15 +0530

    Create StartCoding.cpp

---
+++ GeeksForGeeks/StartCoding.cpp
@@ -0,0 +1,36 @@
+//Start Coding
+//https://www.geeksforgeeks.org/problems/start-coding/1?page=1&difficulty=School,Basic&status=unsolved,attempted&sortBy=accuracy
+//Time Complexity: O(1)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<iostream>
+
+using namespace std;
+
+void printHello();
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to print "Hello World"
+void printHello(){
+    
+    // Your code goes here
+    cout<<"Hello World";
+    
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    // Calling function to print Hello World
+    printHello();
+
+return 0;
+}
+// } Driver Code Ends
