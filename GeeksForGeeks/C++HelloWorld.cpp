dfb0933b34f8efd43b651774e81407c6d1b877fa
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-09 12:18:02 +0530

    Create C++HelloWorld.cpp

---
+++ GeeksForGeeks/C++HelloWorld.cpp
@@ -0,0 +1,33 @@
+//C++ Hello World
+//https://www.geeksforgeeks.org/problems/c-hello-world3402/1?page=1&difficulty=School,Basic&status=unsolved,attempted&sortBy=accuracy
+//Time Complexity: O(1)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        //Type below cout<<"Hello World"; 
+        //cout will output to console
+        cout<<"Hello World"; 
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    Solution ob;
+    //function call
+    ob.helloWorld();
+    cout<<endl;
+    return 0;
+}
+// } Driver Code Ends
