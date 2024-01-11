10db2a6f582b54ddb764a64f9b4115c82c64d082
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-11 15:56:34 +0530

    Create PrintTheLeftElement.java

---
+++ GeeksForGeeks/PrintTheLeftElement.java
@@ -0,0 +1,56 @@
+//Print the left element
+//https://www.geeksforgeeks.org/problems/print-the-left-element2009/1?page=1&difficulty=School&status=unsolved,attempted&sortBy=accuracy
+//Time Complexity: O(n log n)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.leftElement(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public long leftElement(long arr[], long n)
+    {
+        // Your code goes here
+        Arrays.sort(arr);
+        if(n%2==0)
+        {
+            return arr[(int)(n/2-1)]; // even number of terms -> first in the middle pair
+        }
+        return arr[(int)(n/2)]; // odd number of terms -> first in the middle pair
+    }
+}
