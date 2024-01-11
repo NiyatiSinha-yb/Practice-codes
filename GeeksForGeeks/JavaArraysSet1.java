568d4dd09973273e2f65218cdda9438e341775b3
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-11 13:53:54 +0530

    Create JavaArraysSet1.java

---
+++ GeeksForGeeks/JavaArraysSet1.java
@@ -0,0 +1,58 @@
+//Java Arrays | Set 1
+//https://www.geeksforgeeks.org/problems/java-arrays-set-11354/1?page=1&difficulty=School&status=unsolved,attempted&sortBy=accuracy
+//Time Complexity: O(N)
+//Space complexity: O(1)
+
+//{ Driver Code Starts
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class GFG{
+    public static void main (String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		String str;
+		String st[];
+		
+            
+            while(t>0)
+            {
+            	int N = Integer.parseInt(br.readLine());
+            	int sum = 0;
+            	
+            	int A[] = new int[N];
+            	str = br.readLine();
+    			st = str.split(" ");
+            	for (int i = 0; i < N; i++)
+    			{
+    				A[i]= Integer.parseInt(st[i]);
+    				sum+=A[i];
+    			}
+    			Compute ob = new Compute();
+            	String s=ob.average(A, N);
+            	System.out.println(sum+" "+s);
+                t--;
+            }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Compute
+{
+    String average(int A[], int N)
+    {
+        long sum=0;
+        for(int i=0; i<N; i++)
+        {
+            sum+=A[i];
+        }
+        double avg=sum/N;
+        return String.format("%.2f", avg);
+    }
+}
