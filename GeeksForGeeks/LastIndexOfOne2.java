99c724a0b87d2c099959f56253c24a2b460f437a
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-13 19:24:52 +0530

    Create LastIndexOfOne2.java

---
+++ GeeksForGeeks/LastIndexOfOne2.java
@@ -0,0 +1,38 @@
+//Last index of One
+//https://www.geeksforgeeks.org/problems/last-index-of-15847/1?page=2&difficulty=School,Basic&status=unsolved,attempted&sortBy=submissions
+//Time Complexity: O(1)
+//Space complexity: O(1)
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String s = br.readLine();
+
+            Solution obj = new Solution();
+            System.out.println(obj.lastIndex( s));
+
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public int lastIndex( String s) {
+        
+        return (s.lastIndexOf('1'));
+    }
+}
