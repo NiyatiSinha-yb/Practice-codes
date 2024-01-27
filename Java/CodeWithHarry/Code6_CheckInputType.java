7b102c3b58f625e05d80d66ca19984c0bd07d09a
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-27 17:07:59 +0530

    Create Code6_CheckInputType.java

---
+++ Java/CodeWithHarry/Code6_CheckInputType.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println(sc.hasNextLong());
+        
+    }
+}
