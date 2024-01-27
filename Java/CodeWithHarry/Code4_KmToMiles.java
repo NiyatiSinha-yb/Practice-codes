175055011caa399647243a45329af5d307e66b4a
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-27 17:01:31 +0530

    Create Code4_KmToMiles.java

---
+++ Java/CodeWithHarry/Code4_KmToMiles.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Marks are out of 100
+        double km=sc.nextDouble();
+        double miles=km*0.62;
+        System.out.println(km+" kms = "+miles+" miles");
+        
+    }
+}
