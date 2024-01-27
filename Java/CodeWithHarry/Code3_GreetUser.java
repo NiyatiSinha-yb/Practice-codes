a72b41a30e2b5c309e95777ca4c81f1a30fb180d
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-27 16:58:55 +0530

    Create Code3_GreetUser.java

---
+++ Java/CodeWithHarry/Code3_GreetUser.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Marks are out of 100
+        String name=sc.nextLine();
+        System.out.println("Hello "+name+", have a good day!");
+        
+    }
+}
