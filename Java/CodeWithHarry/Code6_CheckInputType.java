4752954231c8ae35618ea0de927b22be98167e21
Niyati Sinha <niyati.sinha29@gmail.com>
2024-01-29 23:45:50 +0530

    Update Code6_CheckInputType.java

--- Java/CodeWithHarry/Code6_CheckInputType.java
+++ Java/CodeWithHarry/Code6_CheckInputType.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
         
+        Scanner sc=new Scanner(System.in);
         System.out.println(sc.hasNextLong());
         
     }
